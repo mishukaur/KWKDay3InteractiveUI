8ce83bf3868eae25205f914d27e66cd715ca6ecb
Manjot Kaur <manjotk2003@gmail.com>
2025-07-09 13:10:38 -0400

    Initial Commit

---
+++ KWKDay3InteractiveUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KWKDay3InteractiveUI
+//
+//  Created by Manjot Kaur on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KWKDay3InteractiveUI/KWKDay3InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKDay3InteractiveUIApp.swift
+//  KWKDay3InteractiveUI
+//
+//  Created by Manjot Kaur on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct KWKDay3InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
